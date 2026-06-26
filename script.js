3fcc1f8114152e4643b1aa5dbfa68e13ed2e0d2c
mastermk106-dotcom <mastermk106@gmail.com>
2026-06-26 11:40:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function showMessage() {
-  alert("Welcome to FF Emote Panel!");
+function claimEmote(){
+
+let uid=document.getElementById("uid").value;
+let server=document.getElementById("server").value;
+
+if(uid==""){
+document.getElementById("result").innerHTML="❌ Please enter your UID";
+return;
+}
+
+document.getElementById("result").innerHTML=
+"✅ UID: "+uid+"<br>🌍 Server: "+server+"<br>🎁 Demo request submitted successfully!";
 }
